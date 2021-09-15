82c7bc6c5bb5febeb775fe4a29a3984814f7666e
Shawn Rice <Shawn.CS@protonmail.com>
2021-09-14 20:27:37 -0400

    boilerplate set-up

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
